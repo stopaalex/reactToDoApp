f31eb27c46a2b6f7bab95de8c619d59dd0792506
Alex Stopa <stopa.alex@gmial.com>
2020-01-24 08:57:37 -0500

    Updates

---
+++ precache-manifest.b16535d511feb5cda3cc2fbadc37dda8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496f424489c9318372e4faca5d02c95e",
+    "url": "/stopaalex/reactToDoApp/index.html"
+  },
+  {
+    "revision": "3c320dcdac5d201c55fb",
+    "url": "/stopaalex/reactToDoApp/static/css/main.fa1609d1.chunk.css"
+  },
+  {
+    "revision": "5696828e9402ab7080e5",
+    "url": "/stopaalex/reactToDoApp/static/js/2.618bf35b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stopaalex/reactToDoApp/static/js/2.618bf35b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c320dcdac5d201c55fb",
+    "url": "/stopaalex/reactToDoApp/static/js/main.5218ff8a.chunk.js"
+  },
+  {
+    "revision": "02f578f8b630fcb7e079",
+    "url": "/stopaalex/reactToDoApp/static/js/runtime-main.36c162c8.js"
+  }
+]);
